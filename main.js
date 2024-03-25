d23918ad8c22333a7be85bdc0800d34939414522
gabriel ferri <107404209+ferrigb@users.noreply.github.com>
2024-03-25 09:00:23 -0300

    mudei css

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-15-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-19-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-21-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-07-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-08-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-09-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-07-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
